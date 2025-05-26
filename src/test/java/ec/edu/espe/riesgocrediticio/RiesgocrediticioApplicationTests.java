f497f38691e36c19c574c0cf50b94ba6f7e20f41
Nahir Carrera <ndcarrera2@espe.edu.ec>
2025-05-26 11:07:31 -0500

    Creacion del proyecto

---
+++ src/test/java/ec/edu/espe/riesgocrediticio/RiesgocrediticioApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.riesgocrediticio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiesgocrediticioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
